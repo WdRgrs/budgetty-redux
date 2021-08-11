eeb2ab9bce160098472476b85e4ef41bf17a22dd
Wade Rogers <83975976+WdRgrs@users.noreply.github.com>
2021-08-10 21:48:30 -0500

    final

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import Background from './../shared/Background/Background'
 import Chart1 from './../shared/Chart1';
@@ -7,27 +8,33 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
-import { connect } from "react-redux";
-import { RequestUserData } from './../../redux/userReducer';
-
+import { connect } from 'react-redux'
+import { requestUserData } from './../../redux/userReducer'
+import { requestBudgetData, addPurchase, removePurchase } from './../../redux/budgetReducer'
 
 
 class Budget extends Component {
+  componentDidMount() {
+    this.props.requestUserData();
+    this.props.requestBudgetData();
+  }
 
   render() {
+    const { loading, purchases, budgetLimit } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase} />
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -36,11 +43,13 @@ class Budget extends Component {
   }
 }
 
-const mapStateToProps = state => (
-  { 
+function mapStateToProps(state) {
+  return {
     budget: state.budget,
     user: state.user
   }
-)
+}
 
-export default connect(mapStateToProps, RequestUserData())(Budget);
+	
+export default connect(mapStateToProps, { requestUserData, requestBudgetData, addPurchase, removePurchase })(Budget);
+
